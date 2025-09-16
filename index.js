88883c26fa79b9d9c622ced6a1aed1d006bcd383
LEon-SIkhDER <leondesktop7580@gmail.com>
2025-09-16 19:21:20 +0600

    basic setup done

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+const port = process.env.PORT || 3000
+
+
+app.get("/", (req, res)=>{
+    res.send("Cooking recipe")
+
+})
+app.listen(port, ()=>{
+    console.log("Cooking Recipe is running on:" , port)
+})
+
+
+// mongoDb 
+//  
+//  
+
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.7hhwads.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const userCollection = client.db("RecipeDb").collection("Users")
+
+    app.get("/users", async(req, res)=>{
+        const result = await userCollection.find().toArray()
+        res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
